1d4bdec1e3a8e92519fbdd295fc7c253cc45fa54
numa dekeyser <numa.dekeyser@epitech.eu>
2022-06-20 09:17:15 +0200

    Ajout d'une route par défaut.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
+  { path: "", component : UserProfileComponent },
   { path: "sign-up", component: SignUpComponent }, 
   { path: "user-profile", component: UserProfileComponent }, 
 ];
