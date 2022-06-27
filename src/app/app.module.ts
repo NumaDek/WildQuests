f68638cc02ca41e55450bdc3fecae828ceac1296
numa dekeyser <numa.dekeyser@epitech.eu>
2022-06-27 10:20:41 +0200

    Bifurcation sur 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMovieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/search-movie-form.model.ts
@@ -0,0 +1,9 @@
+export class searchMovieForm {
+    constructor(
+        // public id: number,
+        // public title: string,
+        // public type: Type,
+        // public year: Date,
+        // public 
+    ) {}
+}
